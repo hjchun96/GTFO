6c06905845cd00026d6acf86f48912448186a4f2
oliverchanyw <oliverchanyw@gmail.com>
2018-12-05 21:24:39 -0500

    combined data

--- cv/combine_training_data.py
+++ cv/combine_training_data.py
@@ -14,15 +14,20 @@ for (path_label, path_img) in zip(glob.glob("../data/cleaned_data/*.png"), glob.
     label = np.array(imageio.imread(path_label))
     img = np.array(imageio.imread(path_img))
 
+
     if img.ndim==3:
         img = rgb2grey(img)
+    if label.ndim == 3:
+        label = rgb2grey(label)
+
+    (x, y) = label.shape
+
 
     # Combine into a r, g, - picture
-    combined = np.zeros(shape=label.shape)
-    combined[:, :, 0] = rgb2grey(label)
+    combined = np.zeros(shape=(x, y, 3))
+    combined[:, :, 0] = label
     combined[:, :, 1] = img
 
     # Rescale to 0-255 and convert to uint8
-    rescaled = (255.0 / combined.max() * (combined - combined.min())).astype(np.uint8)
-    im = Image.fromarray(rescaled)
+    im = Image.fromarray(combined.astype(np.uint8))
     im.save(f'../data/combined_data/{number}.png')
