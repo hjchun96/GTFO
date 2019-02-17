4f666488050958659bd9a5215203d67554131986
Xuerui <faxuerui@seas.upenn.edu>
2019-02-17 15:15:26 -0500

    added storing of images in s3 (TESTING NEEDED)

--- server/src/main/java/com/gtfo/res/BuildingResource.java
+++ server/src/main/java/com/gtfo/res/BuildingResource.java
@@ -4,10 +4,12 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,17 +23,19 @@ public class BuildingResource {
     }
 
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createBuilding(String body) throws JSONException {
+    public Response createBuilding(@FormDataParam("buildingName") String body, @FormDataParam("img") InputStream file)
+            throws JSONException {
         JSONObject json = new JSONObject(body);
         String name = json.getString("name").toLowerCase();
-        JSONArray plans_arr = json.getJSONArray("plans");
-        List<String> plans = new ArrayList<String>();
-        for (int i = 0; i < plans_arr.length(); i++) {
-            plans.add(plans_arr.getString(i));
-        }
-        buildingSvc.createBuilding(name, plans);
+//        JSONArray plans_arr = json.getJSONArray("plans");
+//        List<String> plans = new ArrayList<String>();
+//        for (int i = 0; i < plans_arr.length(); i++) {
+//            plans.add(plans_arr.getString(i));
+//        }
+//        buildingSvc.createBuilding(name, plans);
+        buildingSvc.createBuilding(name, file);
         return Response.ok().build();
     }
 
--- server/src/main/java/com/gtfo/res/BuildingSvc.java
+++ server/src/main/java/com/gtfo/res/BuildingSvc.java
@@ -1,10 +1,14 @@
 package com.gtfo.res;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.gtfo.app.Helpers;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.glassfish.jersey.servlet.internal.Utils;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,16 +17,29 @@ public class BuildingSvc {
     public MongoConnection mongoConnection;
     public MongoDatabase mongoDatabase;
     public MongoCollection<Document> buildingCollection;
+    public S3Connection s3connection;
+    public AmazonS3 s3client;
 
     public BuildingSvc() {
         mongoConnection = MongoConnection.getInstance();
         mongoDatabase = mongoConnection.getDatabase();
         buildingCollection = mongoDatabase.getCollection("buildings");
+
+        s3connection = new S3Connection();
+        s3client = s3connection.getInstance();
     }
 
     public void createBuilding(String name, List<String> plans) {
         Document building = new Document("name", name);
         building.append("plans", name);
+        buildingCollection.insertOne(building);
+    }
+
+    public void createBuilding(String name, InputStream file) {
+        String key = "floorplans/" + name;
+        Helpers.store_in_s3(s3client, key, file);
+        Document building = new Document("name", name);
+        building.append("key", key);
         buildingCollection.insertOne(building);
     }
 
