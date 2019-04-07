5eef7bb5196b228ea1f6b3a184564fac90126d86
Julia Chun <hjchun96@gmail.com>
2019-04-07 16:25:58 -0400

    style updates

--- gtfo/screens/HomeScreen.js
+++ gtfo/screens/HomeScreen.js
@@ -6,7 +6,6 @@ import {
   StyleSheet,
   View,
   Text,
-  // Button,
   TouchableOpacity,
   AsyncStorage,
   StatusBar,
@@ -65,9 +64,9 @@ export default class HomeScreen extends React.Component {
         />
         <Text style={styles.text}>Closest Buildings</Text>
         <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}
-        refreshControl={<RefreshControl refreshing={this.state.refreshing} onRefresh={() => this._getLocationAsync().then(() => this._getClosestBuildings())}/>}
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+          refreshControl={<RefreshControl refreshing={this.state.refreshing} onRefresh={() => this._getLocationAsync().then(() => this._getClosestBuildings())}/>}
         >
           {closestBuildings.length != 0 &&
             closestBuildings.map((building, i) => (
@@ -83,9 +82,10 @@ export default class HomeScreen extends React.Component {
         </ScrollView>
         <Button
           small
-          buttonStyle={styles.item}
+          buttonStyle={styles.addBuildingButton}
           icon={{name: 'plus', type: 'font-awesome'}}
-          title='Add a Building'
+          title='ADD A BUILDING'
+          // color= "#0079C6"
           onPress={() => this._handleAddBuildingButtonPressed()}
           />
       </View>
@@ -95,13 +95,13 @@ export default class HomeScreen extends React.Component {
   _getDistance = (lat1, lon1, lat2, lon2) => {
     var R = 6371; // Radius of the earth in km
     var dLat = this._degreeToRadius(lat2-lat1);  // deg2rad below
-    var dLon = this._degreeToRadius(lon2-lon1); 
-    var a = 
+    var dLon = this._degreeToRadius(lon2-lon1);
+    var a =
       Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.cos(this._degreeToRadius(lat1)) * Math.cos(this._degreeToRadius(lat2)) * 
+      Math.cos(this._degreeToRadius(lat1)) * Math.cos(this._degreeToRadius(lat2)) *
       Math.sin(dLon/2) * Math.sin(dLon/2)
-      ; 
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+      ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     var d = R * c; // Distance in km
     return d;
   }
@@ -184,7 +184,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // paddingTop:10,
   },
   contentContainer: {
     backgroundColor: '#fff',
@@ -210,59 +209,13 @@ const styles = StyleSheet.create({
   homeScreenFilename: {
     marginVertical: 7,
   },
-  // homeScreenFilename: {
-  //   marginVertical: 7,
-  // },
-  // addBuildingButton: {
-  //   position: 'absolute',
-  //   bottom: 0,
-  //   left: 0,
-  //   right: 0,
-  //   ...Platform.select({
-  //     ios: {
-  //       shadowColor: 'black',
-  //       shadowOffset: { height: -3 },
-  //       shadowOpacity: 0.1,
-  //       shadowRadius: 3,
-  //     },
-  //     android: {
-  //       elevation: 20,
-  //     },
-  //   }),
-  //   alignItems: 'center',
-  //   backgroundColor: '#fff',
-  //   paddingVertical: 20,
-  //   paddingBottom: 20,
-  // },
   addBuildingButton: {
-    position: 'absolute',
-    // bottom: 30,
-    // left: 10,
-    // right: 10,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    paddingVertical: 20,
-    paddingBottom: 20,
+    backgroundColor: "#0079C6",
+    marginVertical: 5,
+    borderRadius:10,
+    // width:"70%",
+    alignItems: 'center'
   },
-  // sectionHeader: {
-  //   paddingTop: 2,
-  //   paddingLeft: 10,
-  //   paddingRight: 10,
-  //   paddingBottom: 2,
-  //   fontSize: 14,
-  //   fontWeight: 'bold',
-  //   backgroundColor: 'rgba(247,247,247,1.0)',
-  // },
   listitem: {
     backgroundColor: "#0079C6",
     borderWidth: 3.0,
@@ -275,23 +228,6 @@ const styles = StyleSheet.create({
     borderWidth: 3.0,
     borderColor: '#000000',
     marginVertical: 5,
-
-    // padding: 10,
-    // marginBottom: 15,
-    // fontSize: 30,
-    // height: 44,
-    // ...Platform.select({
-    //     ios: {
-    //       shadowColor: 'black',
-    //       shadowOffset: { height: -3 },
-    //       shadowOpacity: 0.1,
-    //       shadowRadius: 3,
-    //     },
-    //     android: {
-    //       elevation: 20,
-    //     },
-    //   }),
-    // fontFamily: "Roboto",
   },
   header: {
     backgroundColor: "#0079C6",
@@ -302,6 +238,7 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     fontWeight: "800",
     fontSize: 24,
+    fontFamily: 'System',
   },
   loading: {
     backgroundColor: "#0079C6",
